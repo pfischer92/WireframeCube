17445295542d6e04bf1d45c09401bda5b07a20e8
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-11-04 13:07:21 +0100

    Backface Culling implemented

--- CornellBoxWPF/MainWindow.xaml.cs
+++ CornellBoxWPF/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ namespace CornellBoxWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Vector3 camera = new Vector3(0, 0, 0);
         private readonly Stopwatch stopwatch = new Stopwatch();
         private double frameCounter;
         public static WriteableBitmap image { get; set; }
@@ -33,19 +34,19 @@ namespace CornellBoxWPF
                                                                    new Vector3(1,-1,1),
                                                                    new Vector3(1,1,1),
                                                                    new Vector3(-1,1,1)};
-        public static List<Vector3> triangleIdx = new List<Vector3>() {
-            new Vector3(0,1,2),
-            new Vector3(0,2,3),
-            new Vector3(7,6,5),
-            new Vector3(7,5,4),
-            new Vector3(0,3,7),
-            new Vector3(0,7,4),
-            new Vector3(2,1,5),
-            new Vector3(2,5,6),
-            new Vector3(3,2,6),
-            new Vector3(3,6,7),
-            new Vector3(1,0,4),
-            new Vector3(5,4,1)
+        public static List<Triangle> triangles = new List<Triangle>() {
+            new Triangle(new Vector3(0,1,2), new Vector3(1,0,0)),
+            new Triangle(new Vector3(0,2,3),new Vector3(1,0,0)),
+            new Triangle(new Vector3(7,6,5),new Vector3(1,0,0)),
+            new Triangle(new Vector3(7,5,4),new Vector3(1,0,0)),
+            new Triangle(new Vector3(0,3,7),new Vector3(1,0,0)),
+            new Triangle(new Vector3(0,7,4),new Vector3(1,0,0)),
+            new Triangle(new Vector3(2,1,5),new Vector3(1,0,0)),
+            new Triangle(new Vector3(2,5,6),new Vector3(1,0,0)),
+            new Triangle(new Vector3(3,2,6),new Vector3(1,0,0)),
+            new Triangle(new Vector3(3,6,7),new Vector3(1,0,0)),
+            new Triangle(new Vector3(1,0,4),new Vector3(1,0,0)),
+            new Triangle(new Vector3(1,4,5),new Vector3(1,0,0))
         };
 
 
@@ -78,9 +79,10 @@ namespace CornellBoxWPF
         void CompositionTarget_Rendering(object sender, object e)
         {
             CalcFrameRate();
+
             canv.Children.Clear();
-            Polygon p = new Polygon();
-            p.Stroke = Brushes.Black;
+            Polygon polygon = new Polygon();
+            polygon.Stroke = Brushes.Yellow;
             Vector3 v1 = new Vector3(0, 0, 5);
             Point[] trianglePoints = new Point[points.Count];
             List<Vector3> copy = new List<Vector3>(points);
@@ -100,18 +102,27 @@ namespace CornellBoxWPF
                 trianglePoints[i] = new Point((int)(canv.Width * copy[i].X / copy[i].Z + canv.Width / 2), (int)(canv.Width * copy[i].Y / copy[i].Z + canv.Height / 2));
             }
 
-            foreach (var triangle in triangleIdx)
+            foreach (var triangle in triangles)
             {
-                Point p1 = trianglePoints[(int)triangle.X];
-                Point p2 = trianglePoints[(int)triangle.Y];
-                Point p3 = trianglePoints[(int)triangle.Z];
+                // 2D single triangle points
+                Point A = trianglePoints[(int)triangle._pointIdx.X];
+                Point B = trianglePoints[(int)triangle._pointIdx.Y];
+                Point C = trianglePoints[(int)triangle._pointIdx.Z];
 
-                p.Points.Add(p1);
-                p.Points.Add(p2);
-                p.Points.Add(p3);
+                // Backface culling here
+                Vector3 AB = new Vector3((float)(B.X - A.X), (float)(B.Y - A.Y), 0);
+                Vector3 AC = new Vector3((float)(C.X - A.X), (float)(C.Y - A.Y), 0);
+                Vector3 n = Vector3.Normalize(Vector3.Cross(AB, AC));
+                
+                if (n.Z > 0.0f)
+                {
+                    polygon.Points.Add(A);
+                    polygon.Points.Add(B);
+                    polygon.Points.Add(C);
+                }
             }
             
-            canv.Children.Add(p);
+            canv.Children.Add(polygon);
 
 
             //for (int x = 0; x < image.PixelWidth; x++)
---
+++ CornellBoxWPF/Triangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CornellBoxWPF
+{
+    public class Triangle
+    {
+        public Vector3 _pointIdx { get; set; }
+        public Vector3 _color { get; set; }
+
+        public Triangle(Vector3 pointIdx, Vector3 color)
+        {
+            _pointIdx = pointIdx;
+            _color = color;
+        }
+    }
+}
