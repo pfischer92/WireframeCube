689ec0301bb542573d9235ea203c70fa365d5260
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-11-08 00:22:47 +0100

    implement acclereration structure

---
+++ CornellBoxWPF/Helpers/MathHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CornellBoxWPF.Helpers
+{
+    public class MathHelpers
+    {
+        public static int Highest(params int[] inputs)
+        {
+            return inputs.Max();
+        }
+
+        public static int Lowest(params int[] inputs)
+        {
+            return inputs.Min();
+        }
+    }
+}
--- CornellBoxWPF/MainWindow.xaml.cs
+++ CornellBoxWPF/MainWindow.xaml.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using CornellBoxWPF.Helpers;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Numerics;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using WireframeCube.Helpers;
 
 namespace CornellBoxWPF
@@ -92,7 +91,7 @@ namespace CornellBoxWPF
             for (int i = 0; i < points.Count; i++)
             {
                 // Rotate with given matrix
-                points_copy[i] = Vector3.Transform(points_copy[i], MatrixHelpers.GetXRotationMatrix(degree) * MatrixHelpers.GetYRotationMatrix(degree));
+                points_copy[i] = Vector3.Transform(points_copy[i], MatrixHelpers.GetXRotationMatrix(degree)*MatrixHelpers.GetYRotationMatrix(degree));
 
                 // Translate all points
                 points_copy[i] += v1;
@@ -107,39 +106,47 @@ namespace CornellBoxWPF
                 Point A = trianglePoints[(int)triangle._pointIdx.X];
                 Point B = trianglePoints[(int)triangle._pointIdx.Y];
                 Point C = trianglePoints[(int)triangle._pointIdx.Z];
-
+                
                 // Start Backface culling
                 Vector3 AB = new Vector3((float)(B.X - A.X), (float)(B.Y - A.Y), 0);
                 Vector3 AC = new Vector3((float)(C.X - A.X), (float)(C.Y - A.Y), 0);
                 Vector3 n = Vector3.Normalize(Vector3.Cross(AB, AC));
-                
+
+                // Optimization(from inner to outer loop) -> constants for u,v calc
+                float a = AB.X;
+                float b = AC.X;
+                float c = AB.Y;
+                float d = AC.Y;
+                float det = a * d - b * c;
+
+                // Get bounding box
+                int min_x = MathHelpers.Lowest(new int[] { (int)A.X, (int)B.X, (int)C.X });
+                int max_x = MathHelpers.Highest(new int[] { (int)A.X, (int)B.X, (int)C.X });
+                int min_y = MathHelpers.Lowest(new int[] { (int)A.Y, (int)B.Y, (int)C.Y });
+                int max_y = MathHelpers.Highest(new int[] { (int)A.Y, (int)B.Y, (int)C.Y });
+                // End Optimization
+
                 if (n.Z > 0.0f) // Triangle is in front -> Render it
                 {   // End Backface culling
-                    for (int x = 0; x < image.PixelWidth; x++)
+                    for (int x = min_x; x < max_x; x++) // iterate only from min_x to max_x in bounding box
                     {
-                        for (int y = 0; y < image.PixelHeight; y++)
+                        for (int y = min_y; y < max_y; y++) // iterate only from min_x to max_x in bounding box
                         {
-                            // Calc u and v
-                            Vector2 AP = new Vector2(x - (float)A.X, y - (float)A.Y);
-                            float a = AB.X;
-                            float b = AC.X;
-                            float c = AB.Y;
-                            float d = AC.Y;
-                            float det = a * d - b * c;
+                           // Calc u and v
+                           Vector2 AP = new Vector2(x - (float)A.X, y - (float)A.Y);
+                           float upper = AP.X * d + AP.Y * (-b);
+                           float lower = AP.X * (-c) + AP.Y * a;
+                           Vector2 vec = new Vector2(upper * 1 / det, lower * 1 / det);
+                           float u = vec.X;
+                           float v = vec.Y;
 
-                            float upper = AP.X * d + AP.Y * (-b);
-                            float lower = AP.X * (-c) + AP.Y * a;
-                            Vector2 vec = new Vector2(upper * 1 /det, lower * 1/det);
-                            float u = vec.X;
-                            float v = vec.Y;
-
-                            if (u >= 0 && v >= 0 && u + v < 1)
-                            {
-                                color = triangle._color;
-                                colourData[(x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel)] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.X);            // Red
-                                colourData[(x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel + 1)] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.Y);        // Blue
-                                colourData[(x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel + 2)] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.Z);        // Green
-                            }
+                           if (u >= 0 && v >= 0 && u + v < 1)
+                           {
+                               color = triangle._color;
+                               colourData[x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.X);            // Red
+                               colourData[x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel + 1] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.Y);        // Blue
+                               colourData[x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel + 2] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.Z);        // Green
+                           }
                         }
                     }
                 }
