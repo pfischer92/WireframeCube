f668dcf9c9e6dbe6aa17c3b5aa6ab605c63f5a3c
Patrick Fischer <patrick.fischer.privat@gmail.com>
2018-11-09 17:15:43 +0100

    Interpolated colors

--- CornellBoxWPF/MainWindow.xaml.cs
+++ CornellBoxWPF/MainWindow.xaml.cs
@@ -14,42 +14,48 @@ namespace CornellBoxWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Vector3 camera = new Vector3(0, 0, 0);
         private readonly Stopwatch stopwatch = new Stopwatch();
         private double frameCounter;
         public static WriteableBitmap image { get; set; }
         public Vector3 color = new Vector3();
+        public Vector3 light = new Vector3(0, 0, 0);
+        public Vector3 camera = new Vector3(0, 0, 0);
 
         public static byte[] colourData { get; set; }
 
         public static double degree = 0.0;
 
         public static int bytesPerPixel = 3;
-                                                                    // top
-        public static List<Vector3> points = new List<Vector3>() { new Vector3(-1, -1, -1),
-                                                                   new Vector3(1,-1,-1),
-                                                                   new Vector3(1,1,-1),
-                                                                   new Vector3(-1,1,-1),
-                                                                    // bottom
-                                                                   new Vector3(-1,-1,1),
-                                                                   new Vector3(1,-1,1),
-                                                                   new Vector3(1,1,1),
-                                                                   new Vector3(-1,1,1)};
+                                                                      // top
+        public static List<Vector3> cubePoints = new List<Vector3>() {new Vector3(-1, -1, -1),    // A, 0
+                                                                      new Vector3(1,-1,-1),       // B, 1
+                                                                      new Vector3(1,1,-1),        // C, 2
+                                                                      new Vector3(-1,1,-1),       // D, 3
+                                                                     // bottom
+                                                                     new Vector3(-1,-1,1),        // E, 4
+                                                                     new Vector3(1,-1,1),         // F, 5
+                                                                     new Vector3(1,1,1),          // G, 6
+                                                                     new Vector3(-1,1,1)};        // H, 7
+
+
+
         public static List<Triangle> triangles = new List<Triangle>() {
-            new Triangle(new Vector3(0,1,2),new Vector3(1,0,0)),
-            new Triangle(new Vector3(0,2,3),new Vector3(1,0,0)),
-            new Triangle(new Vector3(7,6,5),new Vector3(0,1,0)),
-            new Triangle(new Vector3(7,5,4),new Vector3(0,1,0)),
-            new Triangle(new Vector3(0,3,7),new Vector3(0,0,1)),
-            new Triangle(new Vector3(0,7,4),new Vector3(0,0,1)),
-            new Triangle(new Vector3(2,1,5),new Vector3(1,1,0)),
-            new Triangle(new Vector3(2,5,6),new Vector3(1,1,0)),
-            new Triangle(new Vector3(3,2,6),new Vector3(1,0,1)),
-            new Triangle(new Vector3(3,6,7),new Vector3(1,0,1)),
-            new Triangle(new Vector3(1,0,4),new Vector3(0,1,1)),
-            new Triangle(new Vector3(1,4,5),new Vector3(0,1,1))
+            new Triangle(new Vector3(0,1,2),new Vector3(1,0,0), new Vector4(0,1,0,0), new Vector4(0,0,1,0), new Vector4(1,0,0,0)), 
+            new Triangle(new Vector3(0,2,3),new Vector3(1,0,0), new Vector4(0,1,0,0), new Vector4(1,0,0,0), new Vector4(0,0,1,0)),
+            new Triangle(new Vector3(7,6,5),new Vector3(0,1,0), new Vector4(0,1,0,0), new Vector4(1,0,0,0), new Vector4(0,0,1,0)),
+            new Triangle(new Vector3(7,5,4),new Vector3(0,1,0) ,new Vector4(0,1,0,0), new Vector4(1,0,0,0), new Vector4(0,0,1,0)), 
+            new Triangle(new Vector3(0,3,7),new Vector3(0,0,1), new Vector4(0,1,0,0), new Vector4(0,0,1,0), new Vector4(1,0,0,0)), 
+            new Triangle(new Vector3(0,7,4),new Vector3(0,0,1), new Vector4(0,1,0,0), new Vector4(0,0,1,0), new Vector4(1,0,0,0)),
+            new Triangle(new Vector3(2,1,5),new Vector3(1,1,0), new Vector4(1,0,0,0), new Vector4(0,0,1,0), new Vector4(0,0,1,0)),
+            new Triangle(new Vector3(2,5,6),new Vector3(1,1,0), new Vector4(1,0,0,0), new Vector4(0,0,1,0), new Vector4(0,1,0,0)),
+            new Triangle(new Vector3(3,2,6),new Vector3(1,0,1), new Vector4(0,0,1,0), new Vector4(1,0,0,0), new Vector4(0,1,0,0)),
+            new Triangle(new Vector3(3,6,7),new Vector3(1,0,1) ,new Vector4(0,0,1,0), new Vector4(0,1,0,0), new Vector4(1,0,0,0)),
+            new Triangle(new Vector3(1,0,4),new Vector3(0,1,1), new Vector4(0,0,1,0), new Vector4(0,1,0,0), new Vector4(1,0,0,0)),
+            new Triangle(new Vector3(1,4,5),new Vector3(0,1,1), new Vector4(0,0,1,0), new Vector4(1,0,0,0), new Vector4(0,1,0,0))
         };
 
+        public static Vector3 v1 = new Vector3(0, 0, 5);
+        Point[] trianglePoints = new Point[cubePoints.Count];
 
         public MainWindow()
         {
@@ -80,15 +86,15 @@ namespace CornellBoxWPF
         void CompositionTarget_Rendering(object sender, object e)
         {
             CalcFrameRate();
+
+            // Set and clean up
             colourData = new byte[image.PixelHeight * image.PixelWidth * bytesPerPixel];
-            Vector3 v1 = new Vector3(0, 0, 5);
-            Point[] trianglePoints = new Point[points.Count];
-            List<Vector3> points_copy = new List<Vector3>(points);
+            List<Vector3> points_copy = new List<Vector3>(cubePoints);
 
             // Rotate cube by certain degree
             degree += 0.05f;
 
-            for (int i = 0; i < points.Count; i++)
+            for (int i = 0; i < cubePoints.Count; i++)
             {
                 // Rotate with given matrix
                 points_copy[i] = Vector3.Transform(points_copy[i], MatrixHelpers.GetXRotationMatrix(degree)*MatrixHelpers.GetYRotationMatrix(degree));
@@ -142,7 +148,8 @@ namespace CornellBoxWPF
 
                            if (u >= 0 && v >= 0 && u + v < 1)
                            {
-                               color = triangle._color;
+                               //color = triangle._color;
+                               color = GetInterpolatedColor(triangle, u, v);
                                colourData[x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.X);            // Red
                                colourData[x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel + 1] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.Y);        // Blue
                                colourData[x * bytesPerPixel + y * image.PixelHeight * bytesPerPixel + 2] = GammaCorrection.ConvertAndClampAndGammaCorrect(color.Z);        // Green
@@ -155,9 +162,21 @@ namespace CornellBoxWPF
             image.Lock();
             image.WritePixels(new Int32Rect(0, 0, image.PixelWidth, image.PixelHeight), colourData, image.PixelWidth * bytesPerPixel, 0);
             image.Unlock();
-
             img.Source = image;
         }
+
+        public static Vector3 GetInterpolatedColor(Triangle t, float u, float v)
+        {
+            Vector3 color = Vector3.Zero;
+            Vector3 colorA = new Vector3(t._colorA.X, t._colorA.Y, t._colorA.Z);
+            Vector3 colorB = new Vector3(t._colorB.X, t._colorB.Y, t._colorB.Z);
+            Vector3 colorC = new Vector3(t._colorC.X, t._colorC.Y, t._colorC.Z);
+
+            color = colorA + u * (colorB - colorA) + v * (colorC - colorA);
+
+            return color;
+        }
+
     }
 }
 
--- CornellBoxWPF/Triangle.cs
+++ CornellBoxWPF/Triangle.cs
@@ -11,11 +11,17 @@ namespace CornellBoxWPF
     {
         public Vector3 _pointIdx { get; set; }
         public Vector3 _color { get; set; }
-
-        public Triangle(Vector3 pointIdx, Vector3 color)
+        public Vector4 _colorA { get; set; }
+        public Vector4 _colorB { get; set; }
+        public Vector4 _colorC { get; set; }
+        public Triangle(Vector3 pointIdx, Vector3 fixColor, Vector4 colorA, Vector4 colorB, Vector4 colorC)
         {
             _pointIdx = pointIdx;
-            _color = color;
+            _color = fixColor;
+            _colorA = colorA;
+            _colorB = colorB;
+            _colorC = colorC;
         }
+
     }
 }
